c04f90b66294959318a82477e160e1c4e84f3921
LFinegan-PH502-MP874 <l.finegan2@universityofgalway.ie>
2025-11-16 15:57:36 +0000

    Added practical2

---
+++ practical2/README.md
@@ -0,0 +1,39 @@
+# Practical 2
+
+## Compile and run
+
+### Compile and run `conversion.c` 
+
+```
+gcc conversion.c -o conversion -lm
+./conversion
+```
+
+### Compile and run `sum.c`
+
+```
+gcc sum.c -o sum -lm
+./sum
+```
+
+## Output
+
+### `conversion.c`
+
+```
+inum=6,  fnum=6.000000, inum in binary=110
+The number of digits is 3
+```
+
+```
+inum=622335,  fnum=622335.000000, inum in binary=10010111111011111111
+The number of digits is 20
+```
+
+### `sum.c`
+
+```
+ Sum1=7.485478
+ Sum2=7.485472
+ Difference between the two is 0.000007
+```
---
+++ practical2/conversion.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(void) {
+
+/* Declare variables */
+   int i,inum,tmp,numdigits;
+   float fnum;
+   char binnum[60];
+
+
+/* Intialise 4-byte integer */
+   inum = 6;//33554431;
+/* Convert to 4-byte float */
+   fnum = (float) inum;
+
+
+/* Convert to binary number (string)*/
+   i = 0; tmp = inum;
+   while (tmp > 0) {
+     sprintf(&binnum[i],"%1d",tmp%2);
+     tmp = tmp/2;
+     i++;
+   }
+
+/* Terminate the string */
+   binnum[i] = '\0';
+
+/* Reverse the String binnum*/
+   int length, mid, j;
+   char aux;
+   length = strlen(binnum);
+   mid = length/2;
+   for(i = 0; i < mid; i++) {
+       j = length-i-1;
+       aux = binnum[i];
+       binnum[i] = binnum[j];
+       binnum[j] = aux;
+    }
+
+/* TODO: Complete the expression */
+/*   numdigits = ceil(...);
+   printf("The number of digits is %d\n",numdigits);
+*
+*/
+   if (inum == 0) {
+     numdigits = 1;
+   } else {
+     numdigits = (int) ceil(log2(inum + 1));
+   }
+ 
+ 
+   printf("inum=%d,  fnum=%f, inum in binary=%s\n",inum,fnum,binnum);
+
+   printf("The number of digits is %d\n",numdigits);
+   return 0;
+}
